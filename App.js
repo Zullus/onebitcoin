081be89caeb5a87cb00c895d712825f54741c20a
Edson Santos <edson.santos@gmail.com>
2021-06-09 21:25:17 -0300

    Chamando a API

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { StyleSheet, Text, View, SafeAreaView, StatusBar } from 'react-native';
 
 import CurrentPrice from './src/components/CurrentPrice';
@@ -6,7 +6,111 @@ import HistoryGraphic from './src/components/HistoryGraphic';
 import QuotationList from './src/components/QuotationList';
 import QuotationItens from './src/components/QuotationList/QuotationItens';
 
+function addZero(number){
+
+  if(number <= 9){
+
+    return "0" + number;
+
+  }
+
+  return number;
+
+}
+
+function url(qtdDays){
+
+  const date = new Date();
+
+  const listLastDays = qtdDays;
+
+  const endDate = `${date.getFullYear()}-${addZero(date.getMonth()+1)}-${addZero(date.getDate())}`;
+
+  date.setDate(date.getDate() - listLastDays)
+
+  const startDate = `${date.getFullYear()}-${addZero(date.getMonth()+1)}-${addZero(date.getDate())}`;
+
+  return `https://api.coindesk.com/v1/bpi/historical/close.json?start=${startDate}&end=${endDate}`
+
+}
+
+async function getListCoins(url){
+
+  let response = await fetch(url);
+
+  let returnApi = await response.json();
+
+  let selectListQuotations = returnApi.bpi;
+
+  const queryCoinsList = Object.keys(selectListQuotations).map((key)=>{
+    return{
+      data: key.split("-").reverse().join("/"),
+      valor: selectListQuotations[key]
+    };
+  })
+
+  let data = queryCoinsList.reverse();
+console.log(data)
+  //return data;
+
+}
+
+/*async function getPriceCoinsGraphic(url){
+
+  let responseG = await fetch(url);
+
+  let returnApiG = await responseG.json();
+
+  let selectListQuotationsG = returnApiG.bpi;
+
+  const queryCoinsListG = Object.keys(selectListQuotationsG).map((key)=>{
+      selectListQuotationsG[key]
+  })
+  
+  let dataG = queryCoinsListG;
+console.log(dataG)
+  //return dataG;
+}*/
+
+async function getPriceCoinsGraphic(url) {
+  let responseG = await fetch(url);
+  let returnApiG = await responseG.json();
+  let selectListQuotationsG = returnApiG.bpi;
+  const queryCoinsListG = Object.keys(selectListQuotationsG).map((key) => {
+    return selectListQuotationsG[key];
+  });
+  let dataG = queryCoinsListG;
+  console.log(dataG)
+//  return dataG;
+}
+
 export default function App() {
+
+  const [coinsList, setCoinsList] = useState([]);
+  const [coinsGraphicList, setCoinsGraphicList] = useState([0]); //o gráfico não pode começar vazio
+  const [days, setDays] = useState(30);
+  const [updateData, setUpdateData] = useState(true);
+
+  function updateDay(number){
+    setDays(number);
+    setUpdateData(true);
+  }
+
+  useEffect(() => {
+    getListCoins(url(days)).then((data)=>{
+      setCoinsList(data)
+    });
+
+    getPriceCoinsGraphic(url(days)).then((dataG)=>{
+      setCoinsGraphicList(dataG)
+    });
+
+    if(updateData){
+      setUpdateData(false);
+    }
+
+  }, [updateData])
+
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar
